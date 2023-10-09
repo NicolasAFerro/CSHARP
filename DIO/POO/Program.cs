da447f029b0524228a7ae700fe89909062c13c03
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2023-10-09 14:14:15 -0300

    herança

--- DIO/POO/Program.cs
+++ DIO/POO/Program.cs
@@ -6,10 +6,17 @@ namespace POO{
   class Program{ 
     static void Main(string[] args){ 
       Console.Clear();
-      ContaCorrente c1 = new ContaCorrente(123,1000);
+
+      Aluno a1 = new Aluno(); 
+      a1.Nome="maria";
+      a1.Idade=13;
+      a1.Apresentar();
+      a1.Nota=5.6;
+
+     /*  ContaCorrente c1 = new ContaCorrente(123,1000);
       c1.ExibirSaldo(); 
       c1.Sacar(500); 
-      c1.ExibirSaldo();
+      c1.ExibirSaldo(); */
       //error CS0122: "ContaCorrente.Saldo" é inacessível devido ao seu nível de proteção
       // só altero  saldo atraves do método SACAR;
      /*  //VARIAVEL do tipo pessoa;
