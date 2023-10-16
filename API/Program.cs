78c2cd5a4e7156641adefc75bbd1fda4f1f991c7
NicolasAFerro <nicolasalexandrinoferro@gmail.com>
2023-10-16 14:32:20 -0300

    configurando a conexão

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,16 @@
+using System.Collections.Immutable;
+//CONEXÃO COM O BANCO
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
+builder.Services.AddDbContext<AgendaContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+/* adicioar um DbContext do tipo AgendaContext e passando algumas opções 
+AgendaContext use UseSqlServer (poderia ser outro BANCO)
+getconnectionString(pega alguma chave, por isso colocamos no json) e o nome da minha conexão */
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
